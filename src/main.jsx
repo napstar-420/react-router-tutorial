02f4b5b9eb40285f99dd18f8a4eef33734ebcfb6
zohaib khan <zohaibkhan1162002@gmail>
2022-11-19 15:38:47 +0500

    Import <EditContact /> in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import "./index.css";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import { Children } from "react";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
