db7bbad8b8f52281987c9d7b3a1c09e34c59f2c1
zohaib khan <zohaibkhan1162002@gmail>
2022-11-19 23:31:37 +0500

    stop history of search alphabets

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -43,7 +43,10 @@ export default function Root() {
               name="q"
               defaultValue={q}
               onChange={(e) => {
-                submit(e.currentTarget.form)
+                const isFirstSearch = q == null;
+                submit(e.currentTarget.form, {
+                    replace: !isFirstSearch,
+                })
               }}
             />
             <div id="search-spinner" aria-hidden hidden={!searching} />
