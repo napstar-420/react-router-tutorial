51df0b725dc4863e6ab3cbd6a30e0d5f726e7a2b
zohaib khan <zohaibkhan1162002@gmail>
2022-11-19 17:05:53 +0500

    import useNavigation hook

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLoaderData, Form, redirect, NavLink } from "react-router-dom";
+import { Outlet, useLoaderData, Form, redirect, NavLink, useNavigation } from "react-router-dom";
 import { getContacts, createContact } from "../contact";
 
 export async function loader() {
@@ -13,6 +13,7 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -59,7 +60,7 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail" className={navigation.state === "loading" ? "loading" : ""}>
         <Outlet />
       </div>
     </>
