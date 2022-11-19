83ee763825c13a247829c8b9b129beadcc1ef83c
zohaib khan <zohaibkhan1162002@gmail>
2022-11-19 23:52:59 +0500

    add favorites functionality
    using useFetcher hook and creating an action that will handle the fetcher.form

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,8 +1,15 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contact";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+import { getContact, updateContact } from "../contact";
 
 export async function loader({ params }) {
   return getContact(params.contactId);
+}
+
+export async function action({request, params}) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }
 
 export default function Contact() {
@@ -68,9 +75,13 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -82,6 +93,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
